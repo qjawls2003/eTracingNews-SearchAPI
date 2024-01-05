4b958cc87392f6733adbdf26d51bb5b8f71c66aa
qjawls2003 <qjawls2003@yahoo.co.kr>
2024-01-04 22:58:16 -0500

    init

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .functions.getEmbedding import getEmbedding
+from .functions.getSearch import getSearchResult
 
 app = FastAPI()
 
@@ -10,4 +11,10 @@ def read_root():
 @app.post("/search/")
 def perform_search(query):
     user_query = query.message
-    return {"Search":"Query"}
+    vector = getEmbedding(user_query)
+    result = getSearchResult(vector)
+    return result
+
+
+if __name__ == '__main__':
+    print(perform_search("cybersecurity news for malware"))
