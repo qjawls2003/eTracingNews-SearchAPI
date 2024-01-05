1b30dcc6f82ff182f5a73ddb66f61d8c21f58727
qjawls2003 <qjawls2003@yahoo.co.kr>
2024-01-05 13:38:11 -0500

    fixed post request

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from functions import getEmbedding, getSearch
 from functions.getData import Feeds
+from typing import Union
+from pydantic import BaseModel
+
+class Query(BaseModel):
+    query: str
 
 app = FastAPI()
 
@@ -8,9 +13,9 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/search")
-def perform_search(query):
-    user_query = query.message
+@app.post("/search/")
+async def perform_search(user_query:Query):
+    query = user_query.message
     vector = getEmbedding.getEmbedding(query)
     result = getSearch.getSearchResult(vector)
     print("Length of result: ",len(result))
